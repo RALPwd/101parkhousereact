6a348fb3fe770e12884c37a9745d9d9e53484d73
Raúl Lara <ralpwd@gmail.com>
2022-06-17 10:48:57 -0500

    Si no hay imagen no se muestra el modal

--- components/Constants/api.js
+++ components/Constants/api.js
@@ -1,5 +1,5 @@
-const API_URL = "http://localhost:1337"
-// const API_URL = "https://h101parkhouse.herokuapp.com"
+// const API_URL = "http://localhost:1337"
+const API_URL = "https://h101parkhouse.herokuapp.com"
 
 
 export default API_URL
