500422aa7989e9016f864fed7333f0ddc4396b6d
Raúl Lara <ralpwd@gmail.com>
2022-07-03 03:58:07 -0500

    Creado restaurante

---
+++ components/Constants/restaurant.js
@@ -0,0 +1,24 @@
+import alacarta from '../../public/images/alacarta.png';
+import romantica from '../../public/images/romantica.png';
+import terraza from '../../public/images/terraza.png';
+
+export const restaurant = [
+  {
+    id: 1,
+    title: 'A la carta',
+    background: alacarta,
+    description: '<p> Tenemos nuestro espacio listo para tu almuerzo de negocios, familia o pareja. Nuestro chef complace tu paladar y el de tus acompañantes. </p><br /><p> Encuentra en el restaurante la Mesa de Chef, un espacio reservado para 7 personas donde ofrecemos un servicio personalizado con propuestas de menú especiales para tus preferencias. </p>',
+  },
+  {
+    id: 2,
+    title: 'Cena romántica',
+    background: romantica,
+    description: '<p> Vive una experiencia al 101% en Cassis Restaurante & Cava y crea momentos memorables junto a tu pareja. </p><br /><p> Nos encargamos de los detalles y sorpresas para hacer de este un momento al 101% </p>',
+  },
+  {
+    id: 3,
+    title: 'Terraza',
+    background: terraza,
+    description: '<p> Déjate atrapar por el encanto de nuestros espacios. <br /> Nuestras amplias terrazas son ideales para un brunch al aire libre, una celebración con tu familia o para tardear con los que más amas. </p>',
+  }
+]
